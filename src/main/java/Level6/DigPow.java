3a4bc5e70e2bc13878037fa50b66881998db8467
Mykhailo_Vinnichuk <mishavprogram@ukr.net>
2019-10-28 22:31:11 +0200

    first task. First worked variant

---
+++ src/main/java/Level6/DigPow.java
@@ -0,0 +1,32 @@
+package Level6;
+
+public class DigPow {
+
+    public static long digPow(int n, int p) {
+        int countOfNumbers = 1;
+        int nTemp = n;
+        while (nTemp/10>0){
+            countOfNumbers++;
+            nTemp/=10;
+        }
+        int[] arr = new int[countOfNumbers];
+        nTemp = n;
+        for (int i=countOfNumbers-1; i>=0; i--){
+            int c = nTemp%10;
+            arr[i] = c;
+            nTemp/=10;
+        }
+
+        int s = 0;
+        for (int i=0; i<countOfNumbers; i++){
+            s+=Math.pow(arr[i], p+i);
+        }
+
+        int k = s/n;
+
+        if (k*n==s){
+            return k;
+        }else
+        return -1;
+    }
+}
